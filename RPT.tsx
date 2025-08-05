1744d139355f8a2f2e44d82bb55c242c70cc0232
yonatanrozin <yonatanrozin42@gmail.com>
2025-08-05 14:54:18 -0400

    add preset default gain value

--- RPT.tsx
+++ RPT.tsx
@@ -209,7 +209,7 @@ export class RPT_Voice {
         this.setN(preset.n);
         this.filters.forEach(f => f.gain.value = 0);
         preset.eq?.forEach((f, i) => this.filters[i].gain.value = f);
-        if (preset.gain !== undefined) this.setGain(preset.gain);
+        this.setGain(preset.gain ?? 1);
         if (preset.aspiration != undefined) 
             this.glottis.parameters.get("aspiration")!.value = preset.aspiration;
         this.setPanning(preset.pan || 0);
